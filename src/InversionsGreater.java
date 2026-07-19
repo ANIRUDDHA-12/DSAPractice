8380975755059d03fde7e52cc06607531602c46c
Aniruddha-12 <payas.sanjay@gmail.com>
2026-07-19 21:10:20 +0530

    done with inversions problem

---
+++ src/InversionsGreater.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InversionsGreater {
+    int merge(int [] nums,int low,int mid,int high){
+        int left=low;
+        int right=mid+1;
+        List<Integer> ans=new ArrayList<>();
+        int count=0;
+        while(left<=mid && right<=high){
+            if(nums[left]> 2*(long)nums[right]){
+                count+=mid-left+1;
+                right++;
+            }else{
+                left++;
+            }
+        }
+        left=low;
+        right=mid+1;
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
+                ans.add(nums[left]);
+                left++;
+            }
+            else{
+                ans.add(nums[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            ans.add(nums[left]);
+            left++;
+        }
+        while(right<=high){
+            ans.add(nums[right]);
+            right++;
+        }
+        for(int i=low;i<=high;i++){
+            nums[i]=ans.get(i-low);
+        }
+        return count;
+    }
+    int mergeSort(int [] nums,int low ,int high){
+        if(low>=high) return 0;
+        int mid=(low+high)/2;
+        int count=0;
+        count+=mergeSort(nums,low,mid);
+        count+=mergeSort(nums,mid+1,high);
+        count+=merge(nums,low,mid,high);
+        return count;
+    }
+
+    public int inversionsGreater(int [] nums){
+        if(nums==null || nums.length==0) return 0;
+        return mergeSort(nums,0,nums.length-1);
+    }
+}
